74f4e52c0a48336ae9b322f8cd93e20d255e2255
Xavi Giménez <nottu.2584@gmail.com>
2021-03-05 12:31:13 +0100

    random 6 items from 25

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState([]);
 
-  useEffect(
-    () => {
-      axios
-        .get(
-          "https://api.github.com/repos/facebook/react/contributors"
-        )
-        .then(res => {
-          const newItems = res.data.map(item => {
-            return {
-              id: item.login,
-              avatar: item.avatar_url
-            };
-          })
-          setItems(newItems)
-        });
-    },
-  );
+  const nRandom = (number, arr) => {
+    if (number && number > 1) {
+      const a = [];
+      for (let i = 0; i < number; i++) {
+        a.push(arr[Math.floor(Math.random() * arr.length)]);
+      }
+      return a;
+    } else
+      return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.github.com/repos/facebook/react/contributors?per_page=25&page=1"
+      )
+      .then((res) => {
+        const newItems = nRandom(6, res.data.map((item) => {
+          return {
+            login: item.login,
+            avatar: item.avatar_url,
+          };
+        }));
+        console.log("items", newItems);
+      });
+  });
 
   return (
     <div className="App">
